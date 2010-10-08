a41b7264cf9e46c16d1f69fafe6b7ae1e5719279
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2010-10-09 06:01:47 +0800

    Essentially reverting 908c4bc02b045782403b194d5d97d461d7720ea0 with other minor changes
    In retrospect commit 908c4bc02b045782403b194d5d97d461d7720ea0 wasn't such a good idea.
    
    CampBoard.parse_message() was meant to handle all possibilities in the update object from the server. By inserting return statements as we did, we ended up interrupting the flow of the updates instead.$

--- media/js/campboard.js
+++ media/js/campboard.js
@@ -157,41 +157,38 @@ CampBoard.parse_message = function(d) {
 	
 		if(data['total_tweets']) {
 			$('#total-tweets').html(data['total_tweets']);
-			return;
 		}
 		
 		if(data['uniques']) {
 			$('#unique-tweeters').html(data['uniques']);
-			return;
 		}
 
 		if(data['sessions_number'] >= 0) {
 			$('#total-sessions').html(data['sessions_number']);
-			return;
 		}
 
 		
 		if(data['recent_tweets']) {
 			CampBoard.parse_recent_tweets(data);
-			return;
 		}
 		
 		if(data['sessions']) {
 			CampBoard.parse_sessions(data);
-			return;
 		}	
-		
-		
-		// If we're on a session page
-		if(data['channel'] && document.URL.match(/\/session\/(\w+)/)[1] == data['channel']) {
-			CampBoard.parse_channel(data);
-			return;
-		}
 		
 		if(data['broadcast_message']) {
 			CampBoard.parse_broadcast(data);
-			return;
 		}
+		
+		// If we're on a session page
+		if(data['channel']){
+			var m = document.URL.match(/\/session\/(\w+)/);
+			if(m && m[1] == data['channel']) {
+				CampBoard.parse_channel(data);
+			}
+		}
+		
+
 	}
 	catch(e) {
 		console.log(e)
